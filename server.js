860a9db330ab913dfd6db9a18f828033e6ded7a2
idothestamping <douglas.klemp@gmail.com>
2019-05-01 10:53:19 -0700

    weather working

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 const app = express();
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
@@ -12,17 +14,34 @@ var weatherArr = [];
 
 app.get('/location', (request, response) => {
   try {
-    const locationData = require('./data/geo.json');
     let searchQuery = request.query.data;
-    let formattedAddress = locationData.results[0].formatted_address;
-    let latitude = locationData.results[0].geometry.location.lat;
-    let longitude = locationData.results[0].geometry.location.lng;
+    console.log('searchQuery', searchQuery);
 
-    let locationInstance = new Place(searchQuery, formattedAddress, latitude, longitude);
-    // response.status(200).send(locationInstance);
-    response.send(locationInstance);
-  } catch( error ) {
-    console.log('Sorry, There was an Error');
+    let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${GEOCODE_API_KEY}`;
+
+    superagent.get(geocodeUrl)
+      .end( (err, geocodeUrlRES) => {
+        console.log('data is:', geocodeUrlRES);
+
+        let newgeodata = geocodeUrlRES.body.results.map(data => {
+          let formattedAddress = data.formatted_address;
+          let latitude = data.geometry.location.lat;
+          let longitude = data.geometry.location.lng;
+          return new Place(searchQuery, formattedAddress, latitude, longitude);
+        })
+        response.send(newgeodata);
+      });
+    // const locationData = require('./data/geo.json');
+    // let searchQuery = request.query.data;
+    // let formattedAddress = locationData.results[0].formatted_address;
+    // let latitude = locationData.results[0].geometry.location.lat;
+    // let longitude = locationData.results[0].geometry.location.lng;
+
+    // let locationInstance = new Place(searchQuery, formattedAddress, latitude, longitude);
+    // // response.status(200).send(locationInstance);
+    // response.send(locationInstance);
+  } catch( err ) {
+    console.log('Sorry, There was an Error:', err);
     response.status(500).send('Sorry, There was an Error');
   }
 });
@@ -31,7 +50,7 @@ app.get('/location', (request, response) => {
 app.get('/weather', (request, response) => {
   try {
     const weatherData = require('./data/darksky.json');
-    weatherArr =weatherData.daily.data.map(data => {
+    weatherArr = weatherData.daily.data.map(data => {
       let forecast =  data.summary;
       let time = data.time;
       return new Weather(forecast, time);
@@ -47,15 +66,14 @@ app.get('/weather', (request, response) => {
 
 app.listen(PORT,()=> console.log(`Listening on port ${PORT}`));
 
-function Place (searchQuery, formattedAddress, lat, lng) {
+function Place (searchQuery, formattedAddress, latitude, longitude) {
   this.search_query = searchQuery;
   this.formatted_query = formattedAddress;
-  this.latitude = lat;
-  this.longitude = lng;
+  this.latitude = latitude;
+  this.longitude = longitude;
 }
 
 function Weather (forecast, time) {
   this.forecast = forecast;
   this.time = new Date(time*1000).toDateString();
-  // weatherArr.push(this);
 }
